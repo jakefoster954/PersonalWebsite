a24e1ffd9a0db1934234eb638cba191940ee5f96
Jake Foster <jakefoster954@gmail.com>
2021-08-27 11:03:26 +0100

    menu will automatically close

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelector('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
-})
+});
 
 document.addEventListener('scroll', ()=>{
     var scroll_position = window.scrollY;
@@ -14,4 +15,11 @@ document.addEventListener('scroll', ()=>{
     } else {
         header.style.backgroundColor = 'transparent';
     }
-})
+});
+
+menu_item.forEach(item=>{
+    item.addEventListener('click', ()=>{
+        hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+    });
+});
